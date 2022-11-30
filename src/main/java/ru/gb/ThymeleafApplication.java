c2d37fec6633434adc78f80a4e56d426e5574732
BlackTok <nu.pomytkin@gmail.com>
2022-11-30 10:33:28 +0500

    Initial commit

---
+++ src/main/java/ru/gb/ThymeleafApplication.java
@@ -0,0 +1,18 @@
+package ru.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import ru.gb.controller.ProductController;
+import ru.gb.dao.ProductDao;
+import ru.gb.service.ProductService;
+
+@SpringBootApplication
+@ComponentScan({"ru.gb"})
+public class ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafApplication.class, args);
+
+	}
+}
